d9a5cc03428551373c742124ccd406713fd396eb
GeanCarloslc <geancarloslc@icloud.com>
2022-09-15 20:36:15 -0300

    Desenvolvido o método whenBuscarClienteThenReturnSuccess em whenCreateThenReturnCreated

--- src/test/java/io/github/geancarloslc/api/resources/UsuarioResourceTest.java
+++ src/test/java/io/github/geancarloslc/api/resources/UsuarioResourceTest.java
@@ -84,7 +84,15 @@ class UsuarioResourceTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnCreated() {
+        when(service.create(any())).thenReturn(usuario);
+
+        ResponseEntity<UsuarioDTO> response = resource.create(usuarioDTO);
+
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getHeaders().get("Location"));
+
     }
 
     @Test
